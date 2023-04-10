ef433f492c2de59b4b90e9171575ad154e9ee998
António Silva <antonio.p.fe.silva@gmail.com>
2023-04-10 21:20:11 +0200

    Add MeaningfulGuid type

---
+++ src/SymbolicTypes/MeaningfulGuid.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ShowIntent.SymbolicTypes
+{
+    public abstract class MeaningfulGuid : IMeaningfulType<Guid>
+    {
+        public abstract Guid Value { get; }
+
+        public static bool operator ==(MeaningfulGuid first, MeaningfulGuid second)
+        {
+            if (first is null)
+            {
+                return false;
+            }
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(MeaningfulGuid first, MeaningfulGuid second)
+        {
+            return !(first == second);
+        }
+
+        public bool Equals(MeaningfulGuid other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value == other.Value && this.GetType() == other.GetType();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MeaningfulGuid)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+
+    public abstract class MeaningfulInt : IMeaningfulType<int>
+    {
+        public abstract int Value { get; }
+
+        public static bool operator ==(MeaningfulInt first, MeaningfulInt second)
+        {
+            if (first is null)
+            {
+                return false;
+            }
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(MeaningfulInt first, MeaningfulInt second)
+        {
+            return !(first == second);
+        }
+
+        public bool Equals(MeaningfulInt other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Value == other.Value && this.GetType() == other.GetType();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MeaningfulInt)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
